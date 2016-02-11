95c9b7138508858fe1c5f4c4f1220c423844bb41
Gavin Lee <sz110010@gmail.com>
2016-02-11 14:46:28 +0800

    Add install_vimrc.sh
    used to install .vimrc

---
+++ install_vimrc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ~/.vim
+
+echo 'set runtimepath+=~/.vim
+
+source ~/.vim/vimrcs/basic.vim
+source ~/.vim/vimrcs/filetypes.vim
+source ~/.vim/vimrcs/plugins_config.vim
+source ~/.vim/vimrcs/extended.vim
+
+try
+source ~/.vim/my_configs.vim
+catch
+endtry' > ~/.vimrc
+
+echo "Installed the Ultimate Vim configration successfully!
+Enjoy it! :-)"
