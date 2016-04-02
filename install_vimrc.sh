773bf15ff0f465c48f210c2ac3304096b4975b54
Gavin Lee <sz110010@gmail.com>
2016-04-03 00:29:31 +0800

    Modify install_vimrc

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-cd ~/.vim
+cd ~/.dotfiles/vim
 
 git submodule init
 git submodule update
 
-ln -fs ~/.vim ~/.vim
+ln -fs ~/.dotfiles/vim ~/.vim
 vim +PluginInstall +qall
 yes y | sudo $app install automake gcc gcc-c++ kernel-devel cmake
 yes y | sudo $app install python-devel python3-devel
-cd ~/.vim/bundle/YouCompleteMe
+cd ~/.dotfiles/vim/bundle/YouCompleteMe
 ./install.py --clang-completer --gocode-completer --tern-completer
 ln -fs ~/.vim/ycm_extra_conf.py ~/.ycm_extra_conf.py
