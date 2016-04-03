5bd87e90127ad3a662e93feab7778daeee429144
Gavin Lee <sz110010@gmail.com>
2016-04-03 19:01:02 +0800

    Debug

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -16,6 +16,6 @@ git submodule update
 vim +PluginInstall +qall
 yes y | sudo $app install automake gcc gcc-c++ kernel-devel cmake
 yes y | sudo $app install python-devel python3-devel
-cd ~/$path/bundle/YouCompleteMe
+cd $path/bundle/YouCompleteMe
 ./install.py --clang-completer --gocode-completer --tern-completer
-ln -fs ~/$path/ycm_extra_conf.py ~/.ycm_extra_conf.py
+ln -fs $path/ycm_extra_conf.py ~/.ycm_extra_conf.py
