56cc4abdd75624fb2e43452abc6d0776dc970818
Gavin Lee <sz110010@gmail.com>
2016-04-13 16:33:35 +0800

    Fix install bug

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -3,19 +3,19 @@
 path=$(pwd)
 # checking Linux OS
 if which apt-get >/dev/null 2>&1; then
-    app='apt-get'
+    app='sudo apt-get install'
 elif which dnf >/dev/null 2>&1; then
-    app='dnf'
+    app='sudo dnf install'
 elif which pacman >/dev/null 2>&1; then
-    app='pacman'
+    app='sudo pacman -S'
 fi
 
 git submodule init
 git submodule update
 
 vim +PluginInstall +qall
-yes y | sudo $app install automake gcc gcc-c++ kernel-devel cmake
-yes y | sudo $app install python-devel python3-devel
+yes y | $app automake gcc gcc-c++ kernel-devel cmake
+yes y | $app python-devel python3-devel
 cd $path/bundle/YouCompleteMe
-./install.py --clang-completer --gocode-completer --tern-completer
+./install.py --all
 ln -fs $path/ycm_extra_conf.py ~/.ycm_extra_conf.py
