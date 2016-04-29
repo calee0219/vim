5b4e838b7c32af8eb16695eb66c5f1bec61aabd5
Gavin Lee <sz110010@gmail.com>
2016-04-30 03:31:22 +0800

    Add power line in install

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -10,12 +10,17 @@ elif which pacman >/dev/null 2>&1; then
     app='sudo pacman -S'
 fi
 
+yes | $app automake gcc gcc-c++ kernel-devel cmake
+yes | $app python-devel python3-devel
+yes | $app powerline
+echo 'if [ -d "$HOME/.local/bin"  ]; then
+    PATH="$HOME/.local/bin:$PATH"
+fi' >> ~/.profile
+
 git submodule init
 git submodule update
 
 vim +PluginInstall +qall
-yes y | $app automake gcc gcc-c++ kernel-devel cmake
-yes y | $app python-devel python3-devel
 cd $path/bundle/YouCompleteMe
 ./install.py --all
 ln -fs $path/ycm_extra_conf.py ~/.ycm_extra_conf.py
